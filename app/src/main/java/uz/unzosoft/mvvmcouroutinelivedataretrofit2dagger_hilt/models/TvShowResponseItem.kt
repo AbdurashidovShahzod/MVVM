9d1a4f63e9447c117cdb70a3698eb2fcce3585a1
AbdurashidovShahzod <shahzod9933@gmail.com>
2021-05-30 15:47:49 +0500

    Dependencies implementation

--- app/src/main/java/uz/unzosoft/mvvmcouroutinelivedataretrofit2dagger_hilt/models/TvShowResponseItem.kt
+++ app/src/main/java/uz/unzosoft/mvvmcouroutinelivedataretrofit2dagger_hilt/models/TvShowResponseItem.kt
@@ -1,6 +1,6 @@
 package uz.unzosoft.mvvmcouroutinelivedataretrofit2dagger_hilt.models
 
-data class TvShowResponseItem(C
+data class TvShowResponseItem(
     val id: Int,
     val image: Image,
     val name: String,
